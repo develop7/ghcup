03ac76521ba20faed3b153e08aa575cdd4c4e08f
Andrei Dziahel <develop7@develop7.info>
2020-04-04 13:24:03 +0300

    negate the core condition

--- init.fish
+++ init.fish
@@ -7,7 +7,7 @@
 set -l ghcup_path "$HOME/.ghcup"
 set -l cabalbin_path "$HOME/.cabal/bin"
 
-if contains -- $ghcup_path $PATH 
+if not contains -- $ghcup_path $PATH 
   set -gx PATH "$ghcup_path/bin" $PATH
   contains -- $cabalbin_path $PATH
     or set -gx PATH $cabalbin_path $PATH
