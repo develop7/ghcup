3b01d845f8308dc9eeb25dcb4ea58026357137d8
Andrei Dziahel <develop7@develop7.info>
2020-04-04 13:17:35 +0300

    initial release

---
+++ init.fish
@@ -0,0 +1,14 @@
+# ghcup initialization hook
+#
+# You can use the following variables in this file:
+# * $package       package name
+# * $path          package path
+# * $dependencies  package dependencies
+set -l ghcup_path "$HOME/.ghcup"
+set -l cabalbin_path "$HOME/.cabal/bin"
+
+if contains -- $ghcup_path $PATH 
+  set -gx PATH "$ghcup_path/bin" $PATH
+  contains -- $cabalbin_path $PATH
+    or set -gx PATH $cabalbin_path $PATH
+end
